9ded0156a37817d93996d0e2365280918bfae3ab
Maria-Georgiana Tranca <maria_georgiana9974@yahoo.com>
2022-01-20 19:11:17 +0200

    Implementare meniu principal

--- ProiecITSchoolBank/ProiecITSchoolBank.cpp
+++ ProiecITSchoolBank/ProiecITSchoolBank.cpp
@@ -12,6 +12,32 @@ int main()
 	std::cout << "3 -> Modificare Cont\n";
 	std::cout << "4 -> Stergere Cont\n";
 	std::cout << "5 -> Eliberare/Depunere\n";
-	std::cout << "6 -> Deetalii Cont\n";
-
+	std::cout << "6 -> Detalii Cont\n";
+	int optiune;
+	std::cout << "Introduceti operatia pe care o doriti\n";
+	std::cin >> optiune;
+	switch (optiune)
+	{
+	case 1:
+		std::cout << "Ati ales optiunea 1\n";
+		break;
+	case 2:
+		std::cout << "Ati ales optiunea 2\n";
+		break;
+	case 3:
+		std::cout << "Ati ales optiunea 3\n";
+		break;
+	case 4:
+		std::cout << "Ati ales optiunea 4\n";
+		break;
+	case 5:
+		std::cout << "Ati ales optiunea 5\n";
+		break;
+	case 6:
+		std::cout << "Ati ales optiunea 6\n";
+		break;
+	default:
+		std::cout << "Optiune invalida\n";
+		break;
+	}
 }
